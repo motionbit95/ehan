d10a3812ec21a467c6469e451dcaa2d4b43222bd
motionbit95 <motionbit.dev@gmail.com>
2024-08-08 18:22:20 +0900

    temp

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.get("/bdsm", (req, res) => {
     <meta property="og:description" content="너 SEX MBTI 뭐야? 나의 성적 성향을 알아보세요." />
     <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/ehan-database.appspot.com/o/bdsm.png?alt=media&token=2351a0f7-bbbf-444b-8504-f49fc8022077" />
     <meta property="og:url" content="https://redswitch.kr/bdsm" />`;
+
+  console.log(ogTags);
   fs.readFile(
     path.join(__dirname, "build", "bdsm.html"),
     "utf8",
